0c937f89c7e239dd44b731eb96f46f1c57d282d4
k-arthik-r <karthikclgid@gmail.com>
2024-07-14 18:58:26 +0530

    created gh-pages

--- T4-PatientsDashboard/static/script.js
+++
@@ -1,7 +0,0 @@
-function clearForm() {
-    document.getElementById('name').value = '';
-    document.getElementById('usn').value = '';
-    document.getElementById('grade').value = '';
-    document.getElementById('linkedin').value = '';
-    document.getElementById('github').value = '';
-}
